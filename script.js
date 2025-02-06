c103b5757bb184890ec8a3430dc4e84853fc2426
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-06 11:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	let s_words = s.split(" ")
+	// let[first_word] = s_words;
+	 let first_word = s_words[0];
+	return first_word;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
